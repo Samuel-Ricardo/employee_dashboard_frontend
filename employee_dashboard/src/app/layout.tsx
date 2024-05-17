c53bcefb71dfc0bda5a80cab6b0a5fcd11b2d949
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-17 16:55:49 -0300

    [ :sparkles: ] | create: provider - chakra (app::provider)

--- employee_dashboard/src/app/layout.tsx
+++ employee_dashboard/src/app/layout.tsx
@@ -3,6 +3,7 @@ import 'reflect-metadata';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import { ChakraUIProvider } from '../provider/ui/chakra/chakra.provider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,7 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ChakraUIProvider>{children}</ChakraUIProvider>
+      </body>
     </html>
   );
 }
