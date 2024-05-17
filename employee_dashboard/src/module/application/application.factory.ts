334b9d86dab3533690948dfebcb0913219508907
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-17 15:55:07 -0300

    [ :syringe: ] | update: factory - application > service (app::module)

--- employee_dashboard/src/module/application/application.factory.ts
+++ employee_dashboard/src/module/application/application.factory.ts
@@ -1,7 +1,9 @@
 import { GATEWAY_FACTORY } from './gateway/gateway.factory';
+import { SERVICE_FACTORY } from './service/service.factory';
 import { USE_CASE_FACTORY } from './use_case/use_case.factory';
 
 export const APPLICATION_FACTORY = {
   HTTP: GATEWAY_FACTORY,
   USE_CASE: USE_CASE_FACTORY,
+  SERVICE: SERVICE_FACTORY,
 };
