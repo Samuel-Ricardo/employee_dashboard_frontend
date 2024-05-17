7a4fa9ee4c3e8302313d16d3ac6507f93465e935
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-17 15:35:51 -0300

    [ :syringe: ] | update: factore - application > use-case (app::module)

--- employee_dashboard/src/module/application/application.factory.ts
+++ employee_dashboard/src/module/application/application.factory.ts
@@ -1,5 +1,7 @@
 import { GATEWAY_FACTORY } from './gateway/gateway.factory';
+import { USE_CASE_FACTORY } from './support/use_case/use_case.factory';
 
 export const APPLICATION_FACTORY = {
   HTTP: GATEWAY_FACTORY,
+  USE_CASE: USE_CASE_FACTORY,
 };
