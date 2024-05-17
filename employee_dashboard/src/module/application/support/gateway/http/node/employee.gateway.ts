c44b4a0b854ebafff0c008157ca3fc7dfa1ecb7d
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-17 15:18:26 -0300

    [ :door: ] | create: support - gateway > employee (module::application)

--- employee_dashboard/src/module/application/support/gateway/http/node/employee.gateway.ts
+++ employee_dashboard/src/module/application/support/gateway/http/node/employee.gateway.ts
@@ -1,4 +1,10 @@
+import { MODULE } from '@/module/app.registry';
+import { NodeHttpEmployeeGateway } from '@/module/application/gateway/http/node/employee/employee.gateway';
+import { injectEngine } from '@/module/infra/engine/engine.module';
+import { injectable } from 'inversify';
+
+@injectable()
 export abstract class NodeHttpEmployeeGatewaySupport {
-  @injectEngine(MODULE.INFRA.ENGINE.GATEWAY.HTTP.NODE)
-  protected readonly _engine!: HttpNodeEngine;
+  @injectEngine(MODULE.APPLICATION.GATEWAY.HTTP.NODE.EMPLOYEE)
+  protected readonly _gateway!: NodeHttpEmployeeGateway;
 }
