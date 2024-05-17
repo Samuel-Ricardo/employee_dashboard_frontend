104b4afd7e258effd014f272036bb6eff0dbe708
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-17 17:47:17 -0300

    [ :art: ] | use: component - navbar (app::component)

--- employee_dashboard/src/components/navbar/navbar.component.tsx
+++ employee_dashboard/src/components/navbar/navbar.component.tsx
@@ -3,13 +3,15 @@ import { INavBarProps } from '../../@types/components/navbar/navbar.props';
 
 export const Navbar = async ({ tabs }: INavBarProps) => {
   return (
-    <Tabs>
-      <TabList>
+    <Tabs variant="soft-rounded" colorScheme="red" paddingX={1}>
+      <TabList backgroundColor="#FFF" paddingX={2} borderRadius="0.75rem">
         {tabs.map(tab => (
-          <Tab key={tab.title}>{tab.title}</Tab>
+          <Tab key={tab.title} marginY={2}>
+            {tab.title}
+          </Tab>
         ))}
       </TabList>
-      <TabPanels>
+      <TabPanels paddingX={2}>
         {tabs.map(tab => (
           <TabPanel key={tab.title}>{tab.body}</TabPanel>
         ))}
