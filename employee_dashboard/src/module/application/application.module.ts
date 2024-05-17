22308c49242e5951f05333acef07dda991db1c77
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-17 15:35:39 -0300

    [ :syringe: ] | update: module - application > use-case (app::module)

--- employee_dashboard/src/module/application/application.module.ts
+++ employee_dashboard/src/module/application/application.module.ts
@@ -1,9 +1,14 @@
 import { Container } from 'inversify';
 import { GATEWAY_MODULE } from './gateway/gateway.module';
+import { USE_CASE_MODULE } from './support/use_case/use_case.module';
 
 const _MODULE = new Container({
   autoBindInjectable: true,
   defaultScope: 'Singleton',
 });
 
-export const APPLICATION_MODULE = Container.merge(_MODULE, GATEWAY_MODULE);
+export const APPLICATION_MODULE = Container.merge(
+  _MODULE,
+  GATEWAY_MODULE,
+  USE_CASE_MODULE,
+);
