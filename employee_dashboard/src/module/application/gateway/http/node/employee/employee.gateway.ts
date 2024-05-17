e08ea048021c8f70ab4a41ddfe10c67552075eef
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-17 15:02:38 -0300

    [ :door: ] | impl: gateway - find [all] > employee (module::application)

--- employee_dashboard/src/module/application/gateway/http/node/employee/employee.gateway.ts
+++ employee_dashboard/src/module/application/gateway/http/node/employee/employee.gateway.ts
@@ -1,6 +1,8 @@
 import { NodeHttpGatewaySupport } from '@/module/application/support/gatewy/http/node.support';
 import { ICreateEmployeeDTO } from '@/module/domain/DTO/employee/create.dto';
+import { IEmployeeDTO } from '@/module/domain/DTO/employee/employee.dto';
 import { ICreateEmployeeOutputDTO } from '@/module/domain/DTO/outoput/employee/create.dto';
+import { Employee } from '@/module/domain/entity/employee.entity';
 import { IEmployeeGateway } from '@/module/domain/gateway/employee.gateway';
 import { injectable } from 'inversify';
 
@@ -18,5 +20,12 @@ export class NodeHttpEmployeeGateway
     console.log({ result });
 
     return result as ICreateEmployeeOutputDTO;
+  }
+
+  async findAll() {
+    const response = await this._engine.get(this._url);
+    const result = (await response.json()) as IEmployeeDTO[];
+
+    return result.map(Employee.fromDTO);
   }
 }
