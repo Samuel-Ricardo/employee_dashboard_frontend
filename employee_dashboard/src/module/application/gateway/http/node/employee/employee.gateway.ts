c8e9c2e4be341077a82e9c5bd329b87760d8664c
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-17 15:00:17 -0300

    [ :door: ] | impl: gateway - create > employee (module::application)

--- employee_dashboard/src/module/application/gateway/http/node/employee/employee.gateway.ts
+++ employee_dashboard/src/module/application/gateway/http/node/employee/employee.gateway.ts
@@ -1,10 +1,22 @@
-import { NodeHttpGatewaySupport } from "@/module/application/support/gatewy/http/node.support";
-import { IEmployeeGateway } from "@/module/domain/gateway/employee.gateway";
-import { injectable } from "inversify";
+import { NodeHttpGatewaySupport } from '@/module/application/support/gatewy/http/node.support';
+import { ICreateEmployeeDTO } from '@/module/domain/DTO/employee/create.dto';
+import { ICreateEmployeeOutputDTO } from '@/module/domain/DTO/outoput/employee/create.dto';
+import { IEmployeeGateway } from '@/module/domain/gateway/employee.gateway';
+import { injectable } from 'inversify';
 
 @injectable()
-export class NodeHttpEmployeeGateway implements IEmployeeGateway extends NodeHttpGatewaySupport {
+export class NodeHttpEmployeeGateway
+  extends NodeHttpGatewaySupport
+  implements IEmployeeGateway
+{
+  private readonly _url = 'http://api:3000/api/employees';
 
+  async create(DTO: ICreateEmployeeDTO) {
+    const response = await this._engine.post(this._url, DTO);
+    const result = await response.json();
 
+    console.log({ result });
 
+    return result as ICreateEmployeeOutputDTO;
+  }
 }
