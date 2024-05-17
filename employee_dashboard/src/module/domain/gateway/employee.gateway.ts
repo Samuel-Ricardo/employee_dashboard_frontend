4f67ee1b4055b37edd82d4b01d726d65a8eccaf2
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-17 14:46:11 -0300

    [ :door: ] | create: gateway - employee (module::domain)

---
+++ employee_dashboard/src/module/domain/gateway/employee.gateway.ts
@@ -0,0 +1,14 @@
+import { ICreateEmployeeDTO } from '../DTO/employee/create.dto';
+import { IDeleteEmployeeDTO } from '../DTO/employee/delete.dto';
+import { IFindOneEmployeeDTO } from '../DTO/employee/find/one.dto';
+import { IUpdateEmployeeDTO } from '../DTO/employee/update.dto';
+import { ICreateEmployeeOutputDTO } from '../DTO/outoput/employee/create.dto';
+import { Employee } from '../entity/employee.entity';
+
+export interface IEmployeeGateway {
+  create(DTO: ICreateEmployeeDTO): Promise<ICreateEmployeeOutputDTO>;
+  update(DTO: IUpdateEmployeeDTO): Promise<void>;
+  delete(DTO: IDeleteEmployeeDTO): Promise<void>;
+  findOne(DTO: IFindOneEmployeeDTO): Promise<Employee>;
+  findAll(): Promise<Employee[]>;
+}
