716a5c99689b248cd520167f05eb3fc95ad1c403
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-17 22:11:06 -0300

    [ :syringe: ] | create: registry - valdiation (module::application)

---
+++ employee_dashboard/src/components/form/employee/create/form.component.tsx
@@ -0,0 +1,3 @@
+export const CreateEmployeeForm = () => {
+  return <></>;
+};
---
+++ employee_dashboard/src/module/application/validation/validation.registry.ts
@@ -0,0 +1,11 @@
+export const VALIDATION_REGISTRY = {
+  ZOD: {
+    FORM: {
+      EMPLOYEE: {
+        CREATE: Symbol.for(
+          'MODULE::APPLICATION::VALIDATION::ZOD::FORM::EMPLOYEE::CREATE',
+        ),
+      },
+    },
+  },
+};
