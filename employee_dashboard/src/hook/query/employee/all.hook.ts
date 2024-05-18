b9244320dbc37ef526f5dc4eeed358c3ccd98099
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-18 16:51:15 -0300

    [ :link: ] | create: hook - query > employee (app::hook)

---
+++ employee_dashboard/src/hook/query/employee/all.hook.ts
@@ -0,0 +1,24 @@
+'use client';
+
+import { useQuery } from '@tanstack/react-query';
+import { MODULES } from '../../../module/app.factory';
+import { IEmployeeDTO } from '../../../module/domain/DTO/employee/employee.dto';
+
+export const useEmployees = () => {
+  const MODULE = MODULES.APPLICATION.CONTROLLER.EMPLOYEE();
+
+  const { data, isPending, error } = useQuery<
+    any,
+    any,
+    { result: IEmployeeDTO[] }
+  >({
+    queryFn: async () => await MODULE.findAll(),
+    queryKey: ['employees::all'],
+  });
+
+  return {
+    data,
+    isPending,
+    error,
+  };
+};
