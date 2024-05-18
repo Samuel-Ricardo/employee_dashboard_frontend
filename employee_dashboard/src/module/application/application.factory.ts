7ef8789aa09a1187846c6168703299ae020308b8
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-17 22:14:31 -0300

    [ :syringe: ] | update: factory - application > valdiation (app::module)

--- employee_dashboard/src/module/application/application.factory.ts
+++ employee_dashboard/src/module/application/application.factory.ts
@@ -2,10 +2,12 @@ import { CONTROLLER_FACTORY } from './controller/controller.factory';
 import { GATEWAY_FACTORY } from './gateway/gateway.factory';
 import { SERVICE_FACTORY } from './service/service.factory';
 import { USE_CASE_FACTORY } from './use_case/use_case.factory';
+import { VALIDATION_FACTORY } from './validation/validation.factory';
 
 export const APPLICATION_FACTORY = {
   HTTP: GATEWAY_FACTORY,
   USE_CASE: USE_CASE_FACTORY,
   SERVICE: SERVICE_FACTORY,
   CONTROLLER: CONTROLLER_FACTORY,
+  VALIDAITON: VALIDATION_FACTORY,
 };
