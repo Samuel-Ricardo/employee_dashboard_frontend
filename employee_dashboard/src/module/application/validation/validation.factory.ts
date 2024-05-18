c1ed5d070ed49cb22bd64ad7e52b98d66dfcb825
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-17 23:12:06 -0300

    [ :syringe: ] | update: factory - valdiation > update (module::application)

--- employee_dashboard/src/module/application/validation/validation.factory.ts
+++ employee_dashboard/src/module/application/validation/validation.factory.ts
@@ -1,6 +1,7 @@
 import { VALIDATION_MODULE } from './validation.module';
 import { VALIDATION_REGISTRY } from './validation.registry';
 import { CREATE_EMPLOYEE_FORM_SCHEMA } from './zod/form/employee/create.validation';
+import { UPDATE_EMPLOYEE_FORM_SCHEMA } from './zod/form/employee/update.validation';
 
 export const VALIDATION_FACTORY = {
   ZOD: {
@@ -9,6 +10,11 @@ export const VALIDATION_FACTORY = {
         CREATE: () =>
           VALIDATION_MODULE.get<typeof CREATE_EMPLOYEE_FORM_SCHEMA>(
             VALIDATION_REGISTRY.ZOD.FORM.EMPLOYEE.CREATE,
+          ),
+
+        UPDATE: () =>
+          VALIDATION_MODULE.get<typeof UPDATE_EMPLOYEE_FORM_SCHEMA>(
+            VALIDATION_REGISTRY.ZOD.FORM.EMPLOYEE.UPDATE,
           ),
       },
     },
